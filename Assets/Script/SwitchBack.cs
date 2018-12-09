0eba20252b101eb164a70453758dddbd94706300
ZOO4656 <zoo4656keepkeep@gmail.com>
2018-12-09 15:25:29 +0900

    時間経過により背景変更

---
+++ Assets/Script/SwitchBack.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class SwitchBack : MonoBehaviour {
+
+	public GameObject back_Desert;
+	public GameObject back_SunnyMountain;
+	public GameObject back_RainMountain;
+
+	// private int time_id = StartTime % 5;
+
+	// Update is called once per frame
+	void Update () {
+		float startTime = Time.time % 5;
+		int startTime_id = (int)startTime;	//floatからint型へキャスト
+		switch(startTime_id) {
+			case 0 :
+				back_Desert.SetActive(true);
+				back_SunnyMountain.SetActive(false);
+				back_RainMountain.SetActive(false);
+				break;
+			case 1:
+				back_Desert.SetActive(false);
+				back_SunnyMountain.SetActive(true);
+				back_RainMountain.SetActive(false);
+				break;
+			case 2:
+				back_Desert.SetActive(false);
+				back_SunnyMountain.SetActive(false);
+				back_RainMountain.SetActive(true);
+				break;
+		}
+		Debug.Log(startTime_id);
+	}
+}
---
+++ Assets/Script/WoldClockTime.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoldClockTime : MonoBehaviour {
+	System.Diagnostics.Stopwatch woldTime = new System.Diagnostics.Stopwatch();		//現実正解の時間を計測
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
