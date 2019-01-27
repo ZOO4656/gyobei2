733c5b3df9fe71df37d20193f8065fc01f2400a5
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-01-27 11:29:41 +0900

    StageSelectButtonにより背景変更

---
+++ Assets/Script/SelectStage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectStage : MonoBehaviour {
+
+public GameObject desert;
+public GameObject sunny;
+public GameObject cloudy;
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void selectDesert() {
+		desert.SetActive(true);
+		sunny.SetActive(false);
+		cloudy.SetActive(false);
+	}
+
+	public void selectSunny() {
+		desert.SetActive(false);
+		sunny.SetActive(true);
+		cloudy.SetActive(false);
+	}
+
+	public void selsectCloudy() {
+		desert.SetActive(false);
+		sunny.SetActive(false);
+		cloudy.SetActive(true);
+	}
+}
--- Assets/Script/SwitchBack.cs
+++ Assets/Script/SwitchBack.cs
@@ -13,25 +13,28 @@ public class SwitchBack : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		float startTime = Time.time % 5;
-		int startTime_id = (int)startTime;	//floatからint型へキャスト
-		switch(startTime_id) {
-			case 0 :
-				back_Desert.SetActive(true);
-				back_SunnyMountain.SetActive(false);
-				back_RainMountain.SetActive(false);
-				break;
-			case 1:
-				back_Desert.SetActive(false);
-				back_SunnyMountain.SetActive(true);
-				back_RainMountain.SetActive(false);
-				break;
-			case 2:
-				back_Desert.SetActive(false);
-				back_SunnyMountain.SetActive(false);
-				back_RainMountain.SetActive(true);
-				break;
-		}
-		Debug.Log(startTime_id);
+
+		//背景の切り替えはStageSelectButtonにより切り替えるため一旦時間経過による変更は停止
+		//今後は時間経過による釣れる魚のプール調整や天気を変更する際に使用する。
+		// float startTime = Time.time % 5;
+		// int startTime_id = (int)startTime;	//floatからint型へキャスト
+		// switch(startTime_id) {
+		// 	case 0 :
+		// 		back_Desert.SetActive(true);
+		// 		back_SunnyMountain.SetActive(false);
+		// 		back_RainMountain.SetActive(false);
+		// 		break;
+		// 	case 1:
+		// 		back_Desert.SetActive(false);
+		// 		back_SunnyMountain.SetActive(true);
+		// 		back_RainMountain.SetActive(false);
+		// 		break;
+		// 	case 2:
+		// 		back_Desert.SetActive(false);
+		// 		back_SunnyMountain.SetActive(false);
+		// 		back_RainMountain.SetActive(true);
+		// 		break;
+		// }
+		// Debug.Log(startTime_id);
 	}
 }
