e6f411e1f735e1b7b9bc7755e7f857f2f90bd3f6
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-02-01 06:29:38 +0900

    Dictionary画面追加

---
+++ Assets/Script/ShowDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowDictionary : MonoBehaviour {
+
+
+private bool is_pushDictionary = false;
+
+public GameObject showDictionary;
+//StageSelectが表示されているかどうかを確認
+public GameObject selectWindow;
+
+// selectWindow.GetComponent<ShowStageSelect>().is_pushStageSelect;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(is_pushDictionary) {
+			if(selectWindow.activeSelf){
+				selectWindow.SetActive(false);
+			}
+
+			showDictionary.SetActive(true);
+		}else{
+			showDictionary.SetActive(false);
+		}
+	}
+
+	//Dictionaryボタンを押してたかどうか
+	public void pushDictionary() {
+		if(is_pushDictionary == true) {
+			is_pushDictionary = false;
+		}else{
+			is_pushDictionary = true;
+		}
+	}
+}
---
+++ Assets/Script/ShowStageSelect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowStageSelect : MonoBehaviour {
+
+//StageSelectボタンを押したかどうか
+public bool is_pushStageSelect = false;
+
+//StageSelectWindowを非表示にするにはStageSelectButtunをもう一度押す
+public GameObject StageSelectWindow;
+
+//タッチ判定を持っているConfirmTachを制御
+public GameObject ConfirmTach;
+
+//dictionaryが表示されているかどうかを確認
+public GameObject dictionaryWindow;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(is_pushStageSelect) {
+			//dictionary画面が表示されていれば非表示
+			if(dictionaryWindow.activeSelf) {
+				dictionaryWindow.SetActive(false);
+			}
+
+			ConfirmTach.SetActive(false);
+			StageSelectWindow.SetActive(true);
+		}else {
+			ConfirmTach.SetActive(true);
+			StageSelectWindow.SetActive(false);
+		}
+
+
+	}
+
+	//StageSelectボタンを押してたかどうか
+	public void OnClick() {
+		if(is_pushStageSelect == true) {
+			is_pushStageSelect = false;
+		}else {
+			is_pushStageSelect = true;
+		}
+	}
+}
