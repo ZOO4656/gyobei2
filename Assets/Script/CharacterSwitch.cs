cc43f8d4db2691171281e695938558511faa85c5
ZOO4656 <zoo4656keepkeep@gmail.com>
2019-02-02 18:05:06 +0900

    魚格納用のリスト作成

--- Assets/Script/CharacterSwitch.cs
+++ Assets/Script/CharacterSwitch.cs
@@ -13,8 +13,13 @@ public class CharacterSwitch : MonoBehaviour {
 
 	public bool generateFishLock = true;	//魚の生成時の連続生成防止
 
+	//図鑑登録用のリスト
+	public List<int> fishlist;
+
 	// Use this for initialization
 	void Start () {
+		int[] a = {};
+		fishlist = new List<int>(a);
 		
 	}
 	
@@ -25,12 +30,19 @@ public class CharacterSwitch : MonoBehaviour {
 			CharacterSet2.SetActive(true);
 			// Fish.SetActive(true);
 			if(this.generateFishLock) {
-				int fhishId = Random.Range(0,17);
+				int fishId = Random.Range(0,17);
 				//生成元のプレハブをGameObject型で取得
-				GameObject originFishingFish = (GameObject)Resources.Load ("Prefab/fish" + fhishId);
+				GameObject originFishingFish = (GameObject)Resources.Load ("Prefab/fish" + fishId);
 				//プレハブをもとにインスタンスを生成
 				this.fishingFish = Instantiate (originFishingFish, new Vector3(0.0f,0.0f,0.0f), Quaternion.identity);
 				this.generateFishLock = false;
+
+				//釣れた魚をリストに格納
+				fishlist.Add(fishId);
+				int[] f = fishlist.ToArray();
+				for(int i = 0; i < f.Length; i++) {
+					Debug.Log(f[i]);
+				}
 			}
 		}else {
 			CharacterSet1.SetActive(true);
